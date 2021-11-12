91097bf3b9498d8efb1a412c8f1429299903fc1f
kohansal6396 <mahmoud.kohansal@gmail.com>
2021-11-12 19:15:38 +0330

    Remove Id and Href

--- BlazorTabsSet/Components/TabHeaderOptions.cs
+++ BlazorTabsSet/Components/TabHeaderOptions.cs
@@ -3,8 +3,6 @@ namespace BlazorTabsSet.Components
 {
     public class TabHeaderOptions
     {
-        public string Id { get; set; }
-        public string Href { get; set; }
         public string IconClass { get; set; }
         public string Title { get; set; }
     }
